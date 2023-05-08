ebdeeaed6782fea5f5d529002464ed55624b2e38
danica <danquan@ucdavis.edu>
2023-05-08 16:29:55 -0700

    annotated bibliography and friendslist (part1) uploaded; leaflet example updated

--- toolkit/leaflet/script.js
+++ toolkit/leaflet/script.js
@@ -8,13 +8,22 @@
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 
-    L.marker([34.18346161259565, -118.31218562792391]).addTo(map)
+   L.polygon([
+        [34.18346161259565, -118.31218562792391],
+        [34.146360506170794, -118.25282356774241],
+        [34.19346161259565, -118.29218562792391]
+    ]).addTo(map)
     .bindPopup('<h1>Fun Times During Spring Break: <em>Round1 Bowling & Amusement</em></h1><p>I ate some hot pot with my friend Bella and then we went to this arcade :3 We got to play some Japanese rhythm/dance games, and channel the VOCALOID and JPOP into our souls WAHOOOO ✩ We even got to play one of them for free, which was quite lucky! Bella also won a bear from the claw machine.</p><img src="images/round1.jpg">', {
         maxWidth: "auto"
     });
 
-    L.marker([
-        34.43043735477613, -118.40906299048342]).addTo(map)
+    var circle = L.circle([34.43043735477613, -118.40906299048342], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 1000
+    }).addTo(map)
+    .addTo(map)
     .bindPopup('<h1>Fun Times During Spring Break: <em>Skrunklie Time!</em></h1><p>Here is a Momo in her little ducky jammies!</p><p>She is eternal. She <em>is</em> the moment.</p><img src="images/moosh.jpg">', {
         maxWidth: "auto"
     });
@@ -24,6 +33,11 @@
     .bindPopup('<h1>Fun Times During Spring Break: <em>Requiem: Coffee, Tea, and Fantasy</em></h1><p>I had really wanted to come here and finally did! Requiem is a really cool cafe that has a lot of game themed drinks and food. It is also a very aesthetic location. They had this little grotto the looks like a wisteria forest, but it was reserved when we went so we did not get to sit there... maybe next time >:)</p><img src="images/requiem.jpg">', {
         maxWidth: "auto"
     });
+
+    L.popup()
+    .setLatLng([33.82817962834569, -118.9541941039903])
+    .setContent("Hello! Feel free to click on any of the items on the map.")
+    .openOn(map);
 
     L.marker([
         34.12817962834569, -118.0541941039903]).addTo(map)
